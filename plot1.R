c6d717df1165e403576f598c1f4a59e448d4667f
Aditya Sundararajan <asund005@fiu.edu>
2015-10-07 12:41:10 -0400

    Second commit

--- plot1.R
+++ plot1.R
@@ -1,5 +1,7 @@
 plot1 <- function()
 {
+  ##Read the input file and subset it for just the two days February 1, 2007 and February 2, 2007
+  ##Also convert the date and time values to operable formats
   library(dplyr)
   datas <- tbl_df(read.table("household_power_consumption.txt", header=TRUE, sep= ";", na.strings = c("?","")))
   new_data <- datas
@@ -8,7 +10,11 @@ plot1 <- function()
   sub_data$timetemp <- paste(sub_data$Date, sub_data$Time)
   sub_data$Time <- strptime(sub_data$timetemp, format = "%Y-%m-%d %H:%M:%S")
   sub_data <- sub_data[,c(1,2,10,3:9)]
+  
+  ##Make the plot
   hist(sub_data$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", ylab="Frequency", main="Global Active Power")
+  
+  ##Copy plot to PNG file and close the device
   dev.copy(png,'plot1.png')
   dev.off()
   
